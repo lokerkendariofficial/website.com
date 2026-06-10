3236f46caa36495cecc72cfa6b32c4d75b2d20c7
Loker Kendari Official <lokerkendariofficial@gmail.com>
2026-06-10 11:14:38 +0800

    Create config.js

---
+++ auth/config.js
@@ -0,0 +1,8 @@
+// auth/config.js - Konfigurasi global sistem
+window.APP_CONFIG = {
+  name: 'Loker Kendari Official',
+  version: '2.0',
+  debug: true, // set false di production
+  defaultRedirect: '../index.html',
+  loginPage: '../index.html'
+};
